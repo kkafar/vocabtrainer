b838a15846272e93aee9bc5e6390acf6897e0038
Kacper Kafara <kacperkafara@gmail.com>
2024-12-17 23:06:50 +0100

    Remove response delay

--- frontend/app/query/index.ts
+++ frontend/app/query/index.ts
@@ -10,14 +10,7 @@ export async function fetchWordListQuery(): Promise<VocabEntity[]> {
     }
 
     const payload = await response.json();
-    // return payload.payload;
-    return await new Promise((resolve) => {
-      resolve(new Promise((resolveInner) => {
-        setTimeout(() => {
-          resolveInner(payload.payload);
-        }, 2000);
-      }))
-    })
+    return payload.payload;
   } catch (error) {
     throw new Error(`Failed to fetch wordlist from server with error: ${error}`)
   }
