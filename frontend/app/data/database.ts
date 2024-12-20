9bcb772abee8e2be8f312565561ba7e669350c8e
Kacper Kafara <kacperkafara@gmail.com>
2024-12-20 15:04:01 +0100

    Improve error handling when environment is not setup

--- frontend/app/data/database.ts
+++ frontend/app/data/database.ts
@@ -1,9 +1,10 @@
 import Database, { type Database as DatabaseType } from "better-sqlite3";
+import { isStringBlank } from "../lib/text-util";
 
 export default function createDatabaseConnection(): DatabaseType  {
   const dbPath = process.env.SQLITE_DB_PATH;
 
-  if (dbPath == null) {
+  if (dbPath == null || isStringBlank(dbPath)) {
     throw new Error("Invalid environment setup: missing SQLITE_DB_PATH env variable");
   }
 
