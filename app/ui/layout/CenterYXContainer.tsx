e03926b11da2662ca6e00982e15f9fa32ce1c1fd
Kacper Kafara <kacperkafara@gmail.com>
2025-02-16 19:24:03 +0100

    Extract out CenterX & CenterYContainer

--- app/ui/layout/CenterYXContainer.tsx
+++ app/ui/layout/CenterYXContainer.tsx
@@ -3,12 +3,28 @@ import styles from './styles.module.css';
 
 export type Props = ChildrenProp;
 
-export default async function CenterYXContainer({ children }: Props) {
+export async function CenterYContainer({ children }: Props) {
   return (
     <div className={styles.centerYContainer}>
-      <div className={styles.centerXContainer}>
-        {children}
-      </div>
+      {children}
     </div>
+  )
+}
+
+export async function CenterXContainer({ children }: Props) {
+  return (
+    <div className={styles.centerXContainer}>
+      {children}
+    </div>
+  )
+}
+
+export default async function CenterYXContainer({ children }: Props) {
+  return (
+    <CenterYContainer>
+      <CenterXContainer>
+        {children}
+      </CenterXContainer>
+    </CenterYContainer>
   );
 }
