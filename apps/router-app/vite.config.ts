49291cb60b200853ca900838f50a997c9b751c2a
Kacper Kafara <kacperkafara@gmail.com>
2025-05-19 01:08:37 +0200

    Add tailwindcss plugin to vite

--- apps/router-app/vite.config.ts
+++ apps/router-app/vite.config.ts
@@ -2,10 +2,15 @@ import { defineConfig } from "vite";
 import viteReact from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import { resolve } from "node:path";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [TanStackRouterVite({ autoCodeSplitting: true }), viteReact()],
+  plugins: [
+    TanStackRouterVite({ autoCodeSplitting: true }),
+    viteReact(),
+    tailwindcss(),
+  ],
   test: {
     globals: true,
     environment: "jsdom",
